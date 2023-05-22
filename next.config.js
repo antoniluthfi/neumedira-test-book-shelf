52456131dc419781fedd7d76332403002b2bdb14
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-05-23 03:12:48 +0800

    add configuration for image

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'loremflickr.com',
+        port: '',
+        pathname: '/640/480/**',
+      },
+    ],
+  },
   // SVGR
   webpack(config) {
     config.module.rules.push({
