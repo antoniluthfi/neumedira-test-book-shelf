d0f8d5fee70bbe7db90ca2c38435c25891fe68ff
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-05-23 16:35:53 +0800

    adjust meta tag and layout

---
+++ src/app/child-layout.js
@@ -0,0 +1,41 @@
+"use client";
+
+import { useContext, useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
+import { AppContext } from "@/context/AppContext";
+import Sidebar from "@/components/Sidebar";
+import Navbar from "@/components/Navbar";
+
+export default function ChildLayout({ children }) {
+  const { state } = useContext(AppContext);
+  const pathname = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!pathname) return;
+    if (state.isAuthenticated) {
+      router.replace(pathname === "/login" ? "/" : pathname);
+      return;
+    } else {
+      router.replace("/login");
+      return;
+    }
+  }, [state.isAuthenticated, pathname]);
+
+  return (
+    <div className="bg-[url('/images/login-bg.png')] bg-cover bg-no-repeat w-full h-full">
+      {!!state.isAuthenticated ? (
+        <div className="p-10 flex">
+          <Sidebar />
+
+          <div className="basis-[80%] min-h-screen bg-[#F3F3F7] rounded-br-[10px] rounded-tr-[10px] p-10">
+            <Navbar />
+            {children}
+          </div>
+        </div>
+      ) : (
+        children
+      )}
+    </div>
+  );
+}
