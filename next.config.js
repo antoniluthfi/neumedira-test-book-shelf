ab82de1be1a4aa159905cf8b1df90c8a2a88af62
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-05-23 15:27:15 +0800

    add some configurations

--- next.config.js
+++ next.config.js
@@ -1,12 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/api/login",
+          destination: "https://dummyjson.com/auth/login",
+        },
+        {
+          source: "/api/books",
+          destination:
+            "https://643e8199c72fda4a0bf883df.mockapi.io/api/v1/books",
+        },
+      ],
+    };
+  },
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'loremflickr.com',
-        port: '',
-        pathname: '/640/480/**',
+        protocol: "https",
+        hostname: "loremflickr.com",
+        port: "",
+        pathname: "/640/480/**",
       },
     ],
   },
